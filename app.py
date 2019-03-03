96162f6b731e14055500f327658eaecceffcbff6
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-03-03 12:54:43 +0000

    Added materialize cdn and script links

--- app.py
+++ app.py
@@ -2,35 +2,23 @@
 import os
 #import pymongo
 from flask import Flask, render_template, redirect, request, url_for
-#from flask_pymomgo import PyMongo
-#from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-
-# Test for connection to DB
-
-# MONGODB_URI = os.getenv('MONGO_URI')
-# DBS_NAME = "cuisineAppDB"
-# COLLECTION_NAME = "userRecipes"
-
-# def mongo_connect(url):
-#     try:
-#         conn = pymongo.MongoClient(url)
-#         print("Mongo is connected")
-#         return conn
-#     except pymongo.errors.ConnectionFailure as e:
-#         print("Could not connect to MongoDB: %s") % e
-# app.config["MONGO_DNAME"] = 'milestone3-recipe-app'
-# app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-ghrjb.mongodb.net/cuisineAppDB?retryWrites=true'
-# mongo = PyMongo(app)
-        
-#...........................................................Connect heroku app
 
 app = Flask(__name__)
-    
 
+app.config["MONGO_DNAME"] = 'cuisineAppDB'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-j5cta.mongodb.net/cuisineAppDB?retryWrites=true'
+
+mongo = PyMongo(app)
+
+
+ 
 @app.route('/')
-def index():
-    return "HelloWorld Again!"
+@app.route('/get_cuisine')
+def get_cuisine():
+    return render_template("cuisine.html", recipes=mongo.db.userRecipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
