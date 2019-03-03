3743f04e05acf3cb49d3e2e18bb9d05abf487070
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-03-03 10:37:07 +0000

    Testing app

--- app.py
+++ app.py
@@ -1,46 +1,32 @@
 #  Flask Receipe App
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymomgo import PyMongo
-from bson.objectid import ObjectId
 import pymongo
+from flask import Flask, render_template, redirect, request, url_for
+#from flask_pymomgo import PyMongo
+from bson.objectid import ObjectId
+
 
 # Test for connection to DB
 
-MONGODB_URI = os.getenv('MONGO_URI')
-DBS_NAME = "cuisineAppDB"
-COLLECTION_NAME = "userRecipes"
+# MONGODB_URI = os.getenv('MONGO_URI')
+# DBS_NAME = "cuisineAppDB"
+# COLLECTION_NAME = "userRecipes"
 
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
+# def mongo_connect(url):
+#     try:
+#         conn = pymongo.MongoClient(url)
+#         print("Mongo is connected")
+#         return conn
+#     except pymongo.errors.ConnectionFailure as e:
+#         print("Could not connect to MongoDB: %s") % e
+# app.config["MONGO_DNAME"] = 'milestone3-recipe-app'
+# app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-ghrjb.mongodb.net/cuisineAppDB?retryWrites=true'
+
+# mongo = PyMongo(app)
         
-conn = mongo_connect(MONGODB_URI)  
-
-coll = conn[DBS_NAME][COLLECTION_NAME]
-
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
-    
-# Connecto heroku app
+#...........................................................Connect heroku app
 
 app = Flask(__name__)
-
-app.config["MONGO_DNAME"] = 'milestone3-recipe-app'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-ghrjb.mongodb.net/cuisineAppDB?retryWrites=true'
-
-mongo = PyMongo(app)
-
-@app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.fidn())
     
 
 @app.route('/')
@@ -51,6 +37,51 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
+        
+        
+#.......................start app       
+# def show_menu():
+#     print("")
+#     print("1. Add a record")
+#     print("2. Find a record by name")
+#     print("3. Edit a record")
+#     print("4. Delete a record")
+#     print("5. Exit")
+
+#     option = input("Enter option: ")
+#     return option
+
+
+# def main_loop():
+#     while True:
+#         option = show_menu()
+#         if option == "1":
+#             print("You have selected option 1")
+#         elif option == "2":
+#             print("You have selected option 2")
+#         elif option == "3":
+#             print("You have selected option 3")
+#         elif option == "4":
+#             print("You have selected option 4")
+#         elif option == "5":
+#             conn.close()
+#             break
+#         else:
+#             print("Invalid option")
+#         print("")
+        
+        
+# conn = mongo_connect(MONGODB_URI)  
+
+# coll = conn[DBS_NAME][COLLECTION_NAME]
+
+# main_loop()
+    
+    
+    
+    
+    
+
     
    
     
