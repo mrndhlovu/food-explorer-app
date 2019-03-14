ef196d6016c35f9dcce36e0d74cf87a8810d73b4
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-03-14 00:32:52 +0000

    Add form login and register login need to fix secret key to route to other pages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #  Flask Receipe App
 import os
 #import pymongo
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request,session,url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from datetime import datetime
@@ -15,8 +15,46 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-j5cta.mongodb.ne
 mongo = PyMongo(app)
 
 
- 
-@app.route('/')
+@app.route('/index')
+def index():
+    if 'username' in session:
+        return "You are logged in as " + session ['username']
+    return render_template("register.html")
+
+# check if user is h as a username already if true point to add recipe pag
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    users = mongo.db.usersDB
+    login_user = users.find_one({'name' : request.form['username']})
+    
+    if login_user:
+        
+        return redirect(url_for('index'))
+
+    return 'You need to register!'
+    
+@app.route('/')    
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.usersDB
+        existing_user = users.find_one({'name' : request.form['username']})
+       
+        if existing_user is None:
+            
+            users.insert({'name' : request.form['username'], 'passcode' : request.form['password']})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+if __name__  == '__main__':
+    app.secret_key = '123'
+    app.run(debug=True)
+    
+    
 @app.route('/get_cuisine')
 def get_cuisine():
     return render_template("cuisine.html", recipes=mongo.db.userRecipes.find())
@@ -28,20 +66,14 @@ def add_recipe():
     return render_template("addrecipe.html", categories = category_found)
 
 
-
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    last_modified = datetime.now()
     recipe = mongo.db.userRecipes
-    recipe.insert_one({'last_modified': last_modified})
+
+    last_modified = {'last_modified': datetime.today().strftime('%d, %b %Y')}
+    recipe.insert(last_modified)
     recipe.insert_one(request.form.to_dict())
     return redirect(url_for('get_cuisine'))
-    
-insert_recipe()
-    
-    
-    
-
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
