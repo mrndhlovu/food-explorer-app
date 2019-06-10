d310cda94a16df51dbd7e26ab5a6e70bea8de2c0
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-06-10 20:47:22 +0000

    Changed pymongo import

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #  Flask Receipe App
 import os
 from flask import Flask, render_template, redirect, request,session,url_for, flash
-from flask_pymongo import PyMongo
-import pymongo
+from flask_pymongo import PyMongo,pymongo
 from bson.objectid import ObjectId
 from datetime import datetime
 from operator import attrgetter
