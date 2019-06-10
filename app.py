71d512d103bd104e099f424fc840950de1116e3f
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-06-10 20:36:15 +0000

    Set deub true

--- app.py
+++ app.py
@@ -336,7 +336,7 @@ if __name__  == '__main__':
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
-        debug=False)
+        debug=True)
         
 
 
