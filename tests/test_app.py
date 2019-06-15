50bcb320877b4bbdf70b32902781f10e5b6cbea0
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-06-15 20:59:16 +0000

    Add and aligned filter buttons

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,10 +6,11 @@ sys.path.append('../src')
 
 import pytest
 import sys
-from app import app()
+
 
 module_path = os.path.abspath(os.getcwd())    
 
 if module_path not in sys.path:       
 
     sys.path.append(module_path)
+    
